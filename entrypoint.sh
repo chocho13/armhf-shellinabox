cc35862621a939e5844c1497ef4f86c0c0a36553
chocho13 <darosf@hotmail.fr>
2016-08-21 20:34:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
 set -e
-
 if [ $SIAB_USER ]; then
-    if [ $SIAB_PASS ]; then
-        echo -e "${SIAB_PASS}\n${SIAB_PASS}" | adduser -s /bin/sh $SIAB_USER
-    else
-        echo -e "${SIAB_USER}\n${SIAB_USER}" | adduser -s /bin/sh $SIAB_USER
+    if ! id -u $SIAB_USER > /dev/null 2>&1; then
+        if [ $SIAB_PASS ]; then
+            echo -e "${SIAB_PASS}\n${SIAB_PASS}" | adduser -s /bin/sh $SIAB_USER
+        else
+            echo -e "${SIAB_USER}\n${SIAB_USER}" | adduser -s /bin/sh $SIAB_USER
+        fi
     fi
 else
-    if [ $SIAB_PASS ]; then
-        echo -e "${SIAB_PASS}\n${SIAB_PASS}" | adduser -s /bin/sh shellboxuser
-    else
-        echo -e "shellboxuser\nshellboxuser" | adduser -s /bin/sh shellboxuser
+    if ! id -u shellboxuser > /dev/null 2>&1; then
+        if [ $SIAB_PASS ]; then
+            echo -e "${SIAB_PASS}\n${SIAB_PASS}" | adduser -s /bin/sh shellboxuser
+        else
+            echo -e "shellboxuser\nshellboxuser" | adduser -s /bin/sh shellboxuser
+        fi
     fi
 fi
 COMMAND="shellinaboxd -s /:LOGIN --disable-ssl"
 if [ "$@" = "shellinabox" ]; then
-	echo "Executing: ${COMMAND}"
-	exec ${COMMAND}
+        echo "Executing: ${COMMAND}"
+        exec ${COMMAND}
 else
-	echo "Not executing: ${COMMAND}"
-	echo "Executing: ${@}"
-	exec $@
+        echo "Not executing: ${COMMAND}"
+        echo "Executing: ${@}"
+        exec $@
 fi
